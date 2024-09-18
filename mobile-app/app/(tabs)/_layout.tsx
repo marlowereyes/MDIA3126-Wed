f630e10a644c054a955ccddfcd8e622a77f883af
MarloweReyes <mreyes58@my.bcit.ca>
2024-09-18 13:14:23 -0700

    add tabs and navigation

---
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,46 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Homer',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cheese"
+        options={{
+          title: 'Cheese',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'woman-sharp' : 'woman'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="sandwich"
+        options={{
+          title: 'Sandwich',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash-icon' : 'code-slash-icon'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
