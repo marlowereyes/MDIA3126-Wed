b6d0675eac783852fb7d4a096e76484503748a3e
MarloweReyes <mreyes58@my.bcit.ca>
2024-10-09 11:51:45 -0700

    adding lesson-4-code

--- mobile-app/app/(tabs)/sandwich.tsx
+++ mobile-app/app/(tabs)/sandwich.tsx
@@ -4,7 +4,7 @@ import { Link } from 'expo-router'
 export default function Page() {
   return (
   <View>
-      <Text>This is a page about sandwiches. </Text>
+      <Text className="mx-5 my-20">This is a page about sandwiches. </Text>
       <Link href="/">My Index Page</Link>
       <Link href="/cheese">My Cheese paage</Link>
   </View>
